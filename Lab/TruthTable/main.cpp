4ea968667a6919c937a7e7bc185d4c6c04fbece5
Isaac Kang <ikangrcc@gmail.com>
2016-06-27 12:41:18 -0700

    Signed-off-by: Isaac Kang <ikangrcc@gmail.com>

--- Lab/TruthTable/main.cpp
+++ Lab/TruthTable/main.cpp
@@ -1,8 +1,8 @@
 /* 
  * File:   main.cpp
  * Author: Isaac Kang
- * Created on June 27, 2016, 11:27 AM
- * Purpose:  Truth Table
+ * Created on June 27, 2016, 11:27 PM
+ * Purpose: Truth Table
  */
 
 //System Libraries
@@ -31,24 +31,72 @@ int main(int argc, char** argv) {
     cout<<"!(X&&Y) !X||!Y"<<endl;
     
     //First Row
-    x=true;y=true;
+    x = true; y = true;
     cout<<(x?'T':'F')<<" ";
     cout<<(y?'T':'F')<<"  ";
     cout<<(!x?'T':'F')<<"  ";
     cout<<(!y?'T':'F')<<"   ";
-    cout<<(x||y?'T':'F')<<"  ";
+    cout<<(x||y?'T':'F')<<"   ";
+    cout<<(x&&y?'T':'F')<<"    ";
+    cout<<(x^y?'T':'F')<<"    ";
+    cout<<(x^y^y?'T':'F')<<"      ";
+    cout<<(x^y^x?'T':'F')<<"      ";
+    cout<<(!(x||y)?'T':'F')<<"      ";
+    cout<<(!x&&!y?'T':'F')<<"      ";
+    cout<<(!(x&&y)?'T':'F')<<"      ";
+    cout<<(!x||!y?'T':'F')<<" ";
     cout<<endl;
     
     //Second Row
-    x=true;y=false;
+    x = true; y = false;
     cout<<(x?'T':'F')<<" ";
     cout<<(y?'T':'F')<<"  ";
     cout<<(!x?'T':'F')<<"  ";
     cout<<(!y?'T':'F')<<"   ";
-    cout<<(x||y?'T':'F')<<"  ";
+    cout<<(x||y?'T':'F')<<"   ";
+    cout<<(x&&y?'T':'F')<<"    ";
+    cout<<(x^y?'T':'F')<<"    ";
+    cout<<(x^y^y?'T':'F')<<"      ";
+    cout<<(x^y^x?'T':'F')<<"      ";
+    cout<<(!(x||y)?'T':'F')<<"      ";
+    cout<<(!x&&!y?'T':'F')<<"      ";
+    cout<<(!(x&&y)?'T':'F')<<"      ";
+    cout<<(!x||!y?'T':'F')<<" ";
     cout<<endl;
     
     //Third Row
+    x = false; y = true;
+    cout<<(x?'T':'F')<<" ";
+    cout<<(y?'T':'F')<<"  ";
+    cout<<(!x?'T':'F')<<"  ";
+    cout<<(!y?'T':'F')<<"   ";
+    cout<<(x||y?'T':'F')<<"   ";
+    cout<<(x&&y?'T':'F')<<"    ";
+    cout<<(x^y?'T':'F')<<"    ";
+    cout<<(x^y^y?'T':'F')<<"      ";
+    cout<<(x^y^x?'T':'F')<<"      ";
+    cout<<(!(x||y)?'T':'F')<<"      ";
+    cout<<(!x&&!y?'T':'F')<<"      ";
+    cout<<(!(x&&y)?'T':'F')<<"      ";
+    cout<<(!x||!y?'T':'F')<<" ";
+    cout<<endl;
+    
+    //Fourth Row
+    x = false; y = false;
+    cout<<(x?'T':'F')<<" ";
+    cout<<(y?'T':'F')<<"  ";
+    cout<<(!x?'T':'F')<<"  ";
+    cout<<(!y?'T':'F')<<"   ";
+    cout<<(x||y?'T':'F')<<"   ";
+    cout<<(x&&y?'T':'F')<<"    ";
+    cout<<(x^y?'T':'F')<<"    ";
+    cout<<(x^y^y?'T':'F')<<"      ";
+    cout<<(x^y^x?'T':'F')<<"      ";
+    cout<<(!(x||y)?'T':'F')<<"      ";
+    cout<<(!x&&!y?'T':'F')<<"      ";
+    cout<<(!(x&&y)?'T':'F')<<"      ";
+    cout<<(!x||!y?'T':'F')<<" ";
+    cout<<endl;
     
     //Exit Stage Right!
     return 0;
